d258cd0fb2c806a79a51a5a582e8dc3dce157b7e
naveennn398222-netizen <naveennn398222@gmail.com>
2025-11-27 19:10:57 +0530

    save to branch speed

--- merge.py
+++ merge.py
@@ -1,11 +1,14 @@
-# scores.py  (MAIN / MASTER BRANCH)
+# scores.py  (LOCAL BRANCH)
 import sys
 
-# Read scores from user
 scores = list(map(float, input("Enter scores separated by space: ").split()))
 
 total = sum(scores)
 average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
 
 print("Sum of scores:", total)
 print("Average of scores:", average)
+print("Maximum score:", maximum)
+print("Minimum score:", minimum)
